63f3ab63dde65ab9514e3e58c173f97a8962252d
seanpeng12 <seanpeng12@gmail.com>
2018-12-27 22:58:11 +0800

    更新pdo寫法

--- dbresource/student_insert_result.php
+++ dbresource/student_insert_result.php
@@ -1,16 +1,21 @@
 <?php
-
+$dbhost = 'db.changken.org';
+$dbuser = 'sean';
+$dbpasswd = '12345678';
+$dbname = 'changken_web';
+$dsn = "mysql:host=".$dbhost.";dbname=".$dbname;
 try{
-    $db = new PDO('mysql:dbhost=localhost;dbname=student;charset=utf8', 'root', '96748961');
+    $db = new PDO($dsn,$dbuser,$dbpasswd);
+    $conn->exec("SET CHARACTER SET utf8");
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
 }catch(PDOException $e){
-    echo $e->getMessage();
+    echo "CONNECTION FAILED!：".$e->getMessage();
 }
  
 $res = $db->exec("INSERT INTO `student` (`id`, `name`, `email`, `phone`, `departmentGrade`, `project_id`)
 VALUES
-    ('{$_POST["id"]}','{$_POST["name"]}', '{$_POST["email"]}','{$_POST["phone"]}','{$_POST["departmentGrade"]}','{$_POST["project_id"]}');");
+    ('{$_POST["id"]}','{$_POST["name"]}', '{$_POST["email"]}','{$_POST["phone"]}','{$_POST["departmentGrade"]}',{$_POST["project_id"]});");
 if($res > 0){
     echo "新增成功";
 }else{
